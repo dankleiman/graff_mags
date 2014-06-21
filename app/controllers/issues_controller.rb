eee96953958361433114760cecec7886cf0d4ec8
Dan Kleiman <info@dankleiman.com>
2014-06-21 08:21:19 -0400

    Installed foundation and added issues controller and index view

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,5 @@
+class IssuesController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :issues, only: :index
+
+  root 'issues#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
