e08ee41c069fac5d1564522495b7ee5a641fdcc8
Dan Kleiman <info@dankleiman.com>
2014-06-23 18:27:02 -0400

    Created new issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,9 +7,25 @@ class IssuesController < ApplicationController
   end
 
   def create
+    @issue=Issue.new(issue_params)
+       if @issue.save
+      flash[:notice] = "Successfully added issue."
+      redirect_to issues_path
+    else
+      flash[:notice] = "Could not add issue."
+      render :new
+    end
   end
 
   def show
     @issue = Issue.find(params[:id])
+  end
+
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:magazine, :title, :year, :cover_image, :address, :city, :state, :country)
+
   end
 end
