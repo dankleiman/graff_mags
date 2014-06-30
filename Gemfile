a58a5411df949bd61ce5bf630ec641b2bf780ea3
Dan Kleiman <info@dankleiman.com>
2014-06-30 12:52:16 -0400

    Upload sizes

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem 'rails_12factor', group: :production
 
 gem "fog", "~> 1.3.1"
 
+gem "mini_magick"
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -4,7 +4,7 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production? || Rails.env.development?
@@ -20,12 +20,11 @@ class CoverUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-
-    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  end
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -33,6 +32,17 @@ class CoverUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+
+
+  process :resize_to_fit => [1950, 1950]
+
+  version :thumb do
+    process :resize_to_fill => [200,200]
+  end
+
+  version :medium do
+    process :resize_to_fill => [800, 800]
+  end
 
   # Create different versions of your uploaded files:
   # version :thumb do
