31e044044ee12002a8055c91c570a73f47b96f21
Dan Kleiman <info@dankleiman.com>
2014-06-30 09:55:51 -0400

    Update admin test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   describe 'admin checks' do
     it 'is not an admin if the role is not admin' do
-      user = FactoryGirl.create(:user)
+      user = FactoryGirl.build(:user)
       expect(user).to_not be_is_admin
     end
 
     it 'is an admin if the role is admin' do
-      user = FactoryGirl.create(:user, role: 'admin')
+      user = FactoryGirl.build(:user, role: 'admin')
       expect(user).to be_is_admin
     end
   end
