ac4228b419c18184bb8fd8ab84422395a3803e99
Dan Kleiman <info@dankleiman.com>
2014-07-17 21:31:07 -0400

    Added mailchimp error messages

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,12 +7,12 @@ class ContactFormsController < ApplicationController
     @contact_form = ContactForm.new(params[:contact_form])
     subscribe = params[:contact_form][:subscribe]
     if @contact_form.deliver
-      binding.pry
-      if subscribe == 1
+      if subscribe == "1"
         begin
           Gibbon::API.new(ENV['MAILCHIMP_API_KEY']).lists.subscribe({:id => ENV['MAILCHIMP_LIST_ID'], :email => {:email => params[:contact_form][:email]}, :double_optin => false})
         rescue Gibbon::MailChimpError => e
-          return redirect_to root_path, :flash => { error: e.message }
+          flash[:alert] = e.message
+          return render :new
         end
       end
       flash[:notice] = "Thanks for your message!"
